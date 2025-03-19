f579b01ab9fa22e371c66939c6cd88b962cc6ebb
KieranGliver <kieran.glovjunk@gmail.com>
2025-03-19 01:17:40 -0400

    removed score_display and time_display scenes

--- main.gd
+++ main.gd
@@ -1,10 +1,39 @@
 extends Node2D
 
+@export var round_length: int = 0
 
+var events: Dictionary = {} # events[int] = Callable
+var tick: int = 0
 
+@onready var score_display = $CanvasLayer/ScoreDisplay
 @onready var ammo_display = $CanvasLayer/AmmoDisplay
+@onready var time_display = $CanvasLayer/TimeDisplay
+@onready var text_display = $CanvasLayer/TextDisplay
+
+
+func _ready():
+	start()
+
+
+func _process(delta: float) -> void:
+	if score_display.data != GlobalData.score:
+		score_display.data = GlobalData.score
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_mouse"):
 		if not ammo_display.shoot():
 			get_viewport().set_input_as_handled()
+
+
+func start():
+	time_display.data = round_length
+
+
+func _on_little_hand_timeout() -> void:
+	tick += 1
+	time_display.data = round_length - tick
+	print("Tick: ", tick)
+	if tick > round_length:
+		print("GameOver")
+		GameManager.game_over()
---
+++ text_display.gd
@@ -0,0 +1,23 @@
+extends Control
+
+var go: bool = false:
+	set(value):
+		go_display.visible = value
+		go = value
+var game_over: bool = false:
+	set(value):
+		game_over_display.visible = value
+		game_over = value
+var ready_text: bool = false: # Can't name ready
+	set(value):
+		ready_text_display.visible = value
+		ready_text = value
+var time_up: bool = false:
+	set(value):
+		time_up_display.visible = value
+		go = value
+
+@onready var go_display: TextureRect = $Go
+@onready var game_over_display: TextureRect = $Go
+@onready var ready_text_display: TextureRect = $Go
+@onready var time_up_display: TextureRect = $Go
